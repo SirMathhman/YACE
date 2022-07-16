d07fe5b25e5ca8e6d3b739b11d64fba41030b4a1
SirMathhman <sirmathhman@gmail.com>
2022-07-16 12:51:15 -0400

    Add test for process exiting

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -11,25 +11,40 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationTest {
     @Test
-    void test() {
-        var future = CompletableFuture.runAsync(() -> {
-            var scanner = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("".getBytes())));
+    void exits() {
+        var future = runAsync("exit");
+        assertDoesNotThrow(() -> future.get());
+    }
+
+    private CompletableFuture<Void> runAsync(String input) {
+        return CompletableFuture.runAsync(() -> {
+            var scanner = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
             while (true) {
                 try {
-                    scanner.readLine();
+                    var line = scanner.readLine();
+                    if(line == null) continue;
+                    if (line.equals("exit")) {
+                        return;
+                    }
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
+    }
+
+    @Test
+    void awaits_input() {
+        var future = runAsync("");
 
         assertThrows(TimeoutException.class, () -> {
             try {
-                future.get(1, TimeUnit.MILLISECONDS);
+                future.get(1, TimeUnit.SECONDS);
             } catch (ExecutionException e) {
                 throw e.getCause();
             }
