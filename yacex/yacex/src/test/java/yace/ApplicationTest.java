de20975306dd3b70b485744b7e462c4dec56669b
SirMathhman <sirmathhman@gmail.com>
2022-07-16 12:47:49 -0400

    Add timeout test

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,18 +1,38 @@
 package yace;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Set;
+import java.io.InputStreamReader;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationTest {
+    @Test
+    void test() {
+        var future = CompletableFuture.runAsync(() -> {
+            var scanner = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("".getBytes())));
+            while (true) {
+                try {
+                    scanner.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
+        assertThrows(TimeoutException.class, () -> {
+            try {
+                future.get(1, TimeUnit.MILLISECONDS);
+            } catch (ExecutionException e) {
+                throw e.getCause();
+            }
+        });
+    }
 }
