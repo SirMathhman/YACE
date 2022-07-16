94090d50349effd41e1a58b65370f8c39b966da7
SirMathhman <sirmathhman@gmail.com>
2022-07-16 12:18:11 -0400

    Add test for doing nothing

---
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -0,0 +1,17 @@
+package yace;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ApplicationTest {
+    @Test
+    void invalid() {
+
+    }
+
+    @Test
+    void does_nothing() {
+
+    }
+}
