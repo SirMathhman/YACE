6f0e771ef2b74e96d440ee50060df4ffb278a396
SirMathhman <sirmathhman@gmail.com>
2022-07-16 12:34:01 -0400

    Add more detailed exception information

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -32,11 +32,33 @@ public class ApplicationTest {
     }
 
     private void run(Set<Path> sources) throws IOException {
-        for (Path path : sources) {
-            var input = Files.readString(path);
+        for (var file : sources) {
+            var fullName = file.getFileName().toString();
+            var separator = fullName.indexOf('.');
+            var asAbsolute = file.toAbsolutePath();
+
+            if(separator == -1) {
+                var format = "File '%s' does not have an extension and therefore is not a Java source file.";
+                var message = String.format(format, asAbsolute);
+                throw new IllegalArgumentException(message);
+            }
+
+            var name1 = fullName.substring(0, separator);
+            var extension = fullName.substring(separator + 1);
+            if(extension.equals("java")) {
+                var format = "File '%s' does not have an extension of '.java' and therefore is not a Java source file.";
+                var message = String.format(format, asAbsolute);
+                throw new IllegalArgumentException(message);
+            }
+
+            var input = Files.readString(file);
             if(input.isBlank()) {
-                var format = "Content of file at location '%s' cannot be blank.";
-                var message = String.format(format, path.toAbsolutePath());
+                var format = "Error: Content of file at location '%s' cannot be blank.\n" +
+                             "  Fix: Add class template:\n" +
+                             "\n" +
+                             "class " + name1 + " {\n" +
+                             "}";
+                var message = String.format(format, asAbsolute);
                 throw new RuntimeException(message);
             }
         }
