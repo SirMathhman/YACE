384300041071248dd830507080283dabb6a9f71e
SirMathhman <sirmathhman@gmail.com>
2022-07-24 17:24:30 -0400

    Remove bad code

--- yacex/yacex/src/main/java/yace/Main.java
+++
@@ -1,9 +0,0 @@
-package yace;
-
-import java.nio.file.Paths;
-
-public class Main {
-    public static void main(String[] args) {
-        new Application(Paths.get("."), System.in).run();
-    }
-}
--- yacex/yacex/src/main/java/yace/OperationException.java
+++
@@ -1,7 +0,0 @@
-package yace;
-
-public class OperationException extends RuntimeException {
-    public OperationException(String message) {
-        super(message);
-    }
-}
