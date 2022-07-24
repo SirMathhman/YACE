5fdc89a6a5b8144d76a253f84cd424a5a7ba34de
SirMathhman <sirmathhman@gmail.com>
2022-07-24 18:47:46 -0400

    Add test for file creation

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,8 +1,12 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +15,10 @@ import java.util.concurrent.TimeoutException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
+
+    public static final Path TestFile = Paths.get(".", "Test.java");
+    public static final Path TestAnotherFile = Paths.get(".", "Test1.java");
+
     @Test
     void blocks() {
         var future = CompletableFuture.runAsync(this::run);
@@ -20,6 +28,30 @@ public class ApplicationTest {
     @Test
     void file_create() {
         runWithString("create(\"Test\")");
+        assertTrue(Files.exists(TestFile));
+    }
+
+    @Test
+    void file_create_another() {
+        runWithString("create(\"Test1\")");
+        assertTrue(Files.exists(TestAnotherFile));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(TestFile);
+    }
+
+    @Test
+    void file_create_content() throws IOException {
+        runWithString("create(\"Test\"");
+        assertEquals("class Test {\n}", Files.readString(TestFile));
+    }
+
+    @Test
+    void file_create_content_another() throws IOException {
+        runWithString("create(\"Test1\"");
+        assertEquals("class Test1 {\n}", Files.readString(TestAnotherFile));
     }
 
     @Test
@@ -42,6 +74,12 @@ public class ApplicationTest {
                 var line = reader.readLine();
                 if (line == null) continue;
                 if (line.equals("exit")) return;
+                if(line.startsWith("create(\"")) {
+                    var slice = line.substring("create(\"".length());
+                    var nameEnd = slice.indexOf('\"');
+                    var name = slice.substring(0, nameEnd);
+                    Files.writeString(Paths.get(".", name + ".java"), "class " + name + " {\n}");
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
