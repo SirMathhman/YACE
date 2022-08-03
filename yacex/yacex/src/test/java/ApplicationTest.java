a17dc162690177ba275d93c6cf124c2102a42f82
SirMathhman <sirmathhman@gmail.com>
2022-08-02 22:59:22 -0400

    Require files in argument

--- yacex/yacex/src/test/java/ApplicationTest.java
+++ yacex/yacex/src/test/java/ApplicationTest.java
@@ -1,8 +1,56 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTest {
-    @Test
-    void test() {
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
+public class ApplicationTest {
+
+    private Path directory;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        directory = Files.createTempDirectory("working");
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.walkFileTree(directory, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.deleteIfExists(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.deleteIfExists(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    @Test
+    void nothing() {
+
+    }
+
+    @Test
+    void missing() {
+        Assertions.assertThrows(IOException.class, () -> {
+            run("Empty.java");
+        });
+    }
+
+    private void run(String value) throws IOException {
+        if(!Files.exists(directory.resolve(value))) {
+            throw new IOException();
+        }
     }
 }
