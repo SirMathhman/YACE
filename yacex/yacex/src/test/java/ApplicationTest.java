444f5b07e4dbcf46fa810be2d6a1d8a7998f4534
SirMathhman <sirmathhman@gmail.com>
2022-08-07 16:05:11 -0400

    Add no action

--- yacex/yacex/src/test/java/ApplicationTest.java
+++ yacex/yacex/src/test/java/ApplicationTest.java
@@ -1,24 +1,12 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ApplicationTest {
 
@@ -31,7 +19,7 @@ public class ApplicationTest {
 
     @AfterEach
     void tearDown() throws IOException {
-        Files.walkFileTree(working, new SimpleFileVisitor<>() {
+        Files.walkFileTree(working, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 Files.deleteIfExists(file);
@@ -47,98 +35,7 @@ public class ApplicationTest {
     }
 
     @Test
-    void nothing() throws IOException {
-        run(Collections.emptySet());
-        try (var stream = Files.list(working)) {
-            assertTrue(stream.collect(Collectors.toSet()).isEmpty());
-        }
-    }
-
-    @Test
-    void missing() {
-        assertMissing("Empty.java");
-    }
-
-    private void assertMissing(String... files) {
-        Assertions.assertThrows(IOException.class, () -> {
-            try {
-                run(Set.of(files));
-            } catch (RuntimeException e) {
-                throw e.getCause();
-            }
-        });
-    }
-
-    @Test
-    void missing_multiple() {
-        assertMissing("First.java", "Second.java");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"First", "Second"})
-    void import_simple(String name) throws IOException {
-        assertImport(name, 1);
-    }
-
-    private void assertImport(String name, int countExclusive) throws IOException {
-        var names = IntStream.range(0, countExclusive)
-                .mapToObj(value -> "A" + value)
-                .collect(Collectors.toSet());
-
-        var input = names.stream()
-                .map(value -> String.format("import %s;", value))
-                .collect(Collectors.joining("\n"));
-
-        var sourceName = name + ".java";
-        Files.writeString(working.resolve(sourceName), input);
-        run(Set.of(sourceName));
-
-        var actual = Files.readString(working.resolve(name + ".mgs"));
-        var joinedNames = names.stream().sorted()
-                .collect(Collectors.joining(", ", "{ ", " }"));
-
-        assertEquals("import " + joinedNames, actual);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {2, 3})
-    void import_siblings(int count) throws IOException {
-        assertImport("Test", count + 1);
-    }
-
-    private void run(Set<String> files) {
-        files.forEach(source -> {
-            var sourceFile = working.resolve(source);
-            if (!Files.exists(sourceFile)) {
-                throw new RuntimeException(new IOException());
-            }
-            var separator = source.indexOf('.');
-            try {
-                var nameWithoutExtension = source.substring(0, separator);
-                var targetName = nameWithoutExtension + ".mgs";
-                var target = working.resolve(targetName);
-                var input = Files.readString(sourceFile);
-                var lines = input.split(";");
-
-                var imports = new ArrayList<String>();
-                for (var line : lines) {
-                    var stripped = line.strip();
-
-                    var prefix = "import ";
-                    if (stripped.startsWith(prefix)) {
-                        var name = stripped.substring(prefix.length()).trim();
-                        imports.add(name);
-                    } else {
-                        throw new RuntimeException("Invalid token: " + stripped);
-                    }
-                }
-
-                Files.writeString(target, "import " + imports.stream()
-                        .sorted()
-                        .collect(Collectors.joining(", ", "{ ", " }")));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    void test() {
+        assertFalse(Files.exists(working.resolve("First.mgs")));
     }
 }
