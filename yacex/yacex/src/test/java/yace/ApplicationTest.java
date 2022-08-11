36c97faf735763661fc76537c29c638bba4bce32
SirMathhman <sirmathhman@gmail.com>
2022-08-11 14:33:01 -0400

    Set up and tear down working directory

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,8 +1,42 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class ApplicationTest {
+
+    private Path working;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        working = Files.createTempDirectory("working");
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.walkFileTree(working, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.deleteIfExists(dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.deleteIfExists(file);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
     @Test
     void test() {
 
