a46092364ea3d2e41bfae840f149dc4715c88196
SirMathhman <sirmathhman@gmail.com>
2022-08-20 16:43:56 -0400

    Ensure target does not generate when no input given

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -2,9 +2,16 @@ package yace;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class ApplicationTest {
     @Test
-    void test() {
-
+    void generate_target_not() {
+        assertFalse(Files.exists(Paths.get(".", "Index.java")));
     }
+
+
 }
