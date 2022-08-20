0889a824e3711a267e3424263c4469ce34bc7139
SirMathhman <sirmathhman@gmail.com>
2022-08-20 16:48:10 -0400

    Add test for no formatting

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,17 +1,49 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
-    @Test
-    void generate_target_not() {
-        assertFalse(Files.exists(Paths.get(".", "Index.java")));
+    private Path working;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        working = Files.createTempDirectory("working");
     }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.walkFileTree(working, new DeletingVisitor());
+    }
 
+    @Test
+    void format_nothing() throws IOException {
+        var source = working.resolve("Index.java");
+        Files.createFile(source);
+        assertEquals("", Files.readString(source));
+    }
+
+    private static class DeletingVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.delete(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
