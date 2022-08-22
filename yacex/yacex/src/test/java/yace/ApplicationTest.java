67ce4338539c2cfbe481642d5671746a8d165ea2
SirMathhman <sirmathhman@gmail.com>
2022-08-22 19:02:52 -0400

    Add setup code

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,10 +1,47 @@
 package yace;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Optional;
 
 public class ApplicationTest {
-    @Test
-    void test() {
+    private Optional<Path> workingDirectory = Optional.empty();
 
+    @BeforeEach
+    void setUp() throws IOException {
+        workingDirectory = Optional.of(Files.createTempDirectory("working"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        workingDirectory.ifPresent(path -> {
+            try {
+                Files.walkFileTree(path, new DeletingVisitor());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+    private static class DeletingVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.deleteIfExists(dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.deleteIfExists(file);
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
