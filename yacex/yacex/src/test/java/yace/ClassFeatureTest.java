ca7ca162638939721ac7870f6b25c11836e6e12a
SirMathhman <sirmathhman@gmail.com>
2022-08-22 22:58:12 -0400

    Remove duplicates

--- yacex/yacex/src/test/java/yace/ClassFeatureTest.java
+++ yacex/yacex/src/test/java/yace/ClassFeatureTest.java
@@ -5,36 +5,40 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ClassFeatureTest extends ApplicationFeatureTest {
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2})
-    void class_name_format_leading_whitespace(int spacing) {
-        assertFormat(createSpacing(spacing) + " class", "class");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2})
-    void class_name_format_trailing_whitespace(int spacing) {
-        assertFormat(" class" + createSpacing(spacing), "class");
+    private static String renderClass(int prefix, int suffix) {
+        return createSpacing(prefix) + " class" + createSpacing(suffix);
     }
 
     private static String createSpacing(int spacing) {
         return " ".repeat(spacing);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void class_name_format_leading_whitespace(int spacing) {
+        assertFormat(renderClass(spacing, 0), "class");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void class_name_format_trailing_whitespace(int spacing) {
+        assertFormat(renderClass(0, spacing), renderClass(0, 0));
+    }
+
     @Test
-    void class_name_analyze(){
-        assertAnalyzeError("class", path -> new ClassStructureError("class"));
+    void class_name_analyze() {
+        assertAnalyzeError(renderClass(0, 0), path -> new ClassStructureError(renderClass(0, 0)));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2})
     void class_name_analyze_leading_whitespace(int spacing) {
-        assertAnalyzeError(createSpacing(spacing) + "class", path -> new ClassStructureError("class"));
+        assertAnalyzeError(renderClass(spacing, 0), path -> new ClassStructureError(renderClass(0, 0)));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2})
     void class_name_analyze_trailing_whitespace(int spacing) {
-        assertAnalyzeError("class" + createSpacing(spacing), path -> new ClassStructureError("class"));
+        assertAnalyzeError(renderClass(0, spacing), path -> new ClassStructureError(renderClass(0, 0)));
     }
 }
