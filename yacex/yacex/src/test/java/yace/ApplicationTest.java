b956e9f1cf2851336891bf93229b85cd7996ef3e
SirMathhman <sirmathhman@gmail.com>
2022-08-26 22:42:05 -0400

    Setup working directory

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,10 +1,52 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Optional;
+
 public class ApplicationTest {
+    private Optional<Path> working = Optional.empty();
+
+    @BeforeEach
+    void setUp() throws IOException {
+        working = Optional.of(Files.createTempDirectory("working"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        working.ifPresent(value -> {
+            try {
+                Files.walkFileTree(value, new DeletingVisitor());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     @Test
     void test() {
 
+    }
+
+    private static class DeletingVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.delete(file);
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
