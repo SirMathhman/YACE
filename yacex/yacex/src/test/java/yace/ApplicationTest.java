8a5519199d3a7b6ff0f23d687fc7f1c28ad50af2
SirMathhman <sirmathhman@gmail.com>
2022-08-28 17:22:44 -0400

    Setup working directory

--- yacex/yacex/src/test/java/yace/ApplicationTest.java
+++ yacex/yacex/src/test/java/yace/ApplicationTest.java
@@ -1,8 +1,33 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
 public class ApplicationTest {
+    private Optional<Path> working = Optional.empty();
+
+    @BeforeEach
+    void setUp() throws IOException {
+        working = Optional.of(Files.createTempDirectory("working"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        working.ifPresent(value -> {
+            try {
+                Files.walkFileTree(value, new DeletingVisitor());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     @Test
     void test() {
 
