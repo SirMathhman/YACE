f08048689ab2b6183ed6b63279fa4ee4b4314e6d
SirMathhman <sirmathhman@gmail.com>
2022-08-31 18:26:06 -0400

    Add documentation to Gateway

--- yacex/yacex/src/main/java/yace/Gateway.java
+++ yacex/yacex/src/main/java/yace/Gateway.java
@@ -3,6 +3,13 @@ package yace;
 import java.nio.file.Path;
 import java.util.Set;
 
+/**
+ * Represents an object which offers passage between code and an external I/O system.
+ */
 public interface Gateway {
+    /**
+     * Collects all the sources present.
+     * @return The sources.
+     */
     Set<Path> collectSources();
 }
