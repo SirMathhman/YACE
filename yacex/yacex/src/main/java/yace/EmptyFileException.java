1a8352fa98ba79e7538a96152a6a3235400dbbc6
SirMathhman <sirmathhman@gmail.com>
2022-08-31 17:58:50 -0400

    Remove bad code

--- yacex/yacex/src/main/java/yace/EmptyFileException.java
+++
@@ -1,8 +0,0 @@
-package yace;
-
-/**
- * Indicates that the given input file was empty
- * and that empty files are not permissible for the given context.
- */
-public class EmptyFileException extends ApplicationException {
-}
