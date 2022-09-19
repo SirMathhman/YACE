a4e36afc8679535ecd44b6cb4edfeaad674d158c
SirMathhman <SirMathhman@gmail.com>
2022-09-19 12:38:00 -0400

    YACE-7 Add basic test

--- src/test/java/yace/ApplicationTest.java
+++ src/test/java/yace/ApplicationTest.java
@@ -1,10 +1,51 @@
 package yace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Optional;
+
 public class ApplicationTest {
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private Optional<Path> working = Optional.empty();
+
+    @BeforeEach
+    void setUp() throws IOException {
+        working = Optional.of(Files.createTempDirectory("working"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        working.ifPresent(value -> {
+            try {
+                Files.walkFileTree(value, new DeletingVisitor());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     @Test
     void test(){
+        Assertions.assertFalse(Files.exists(working.orElseThrow().resolve("Index.mgs")));
+    }
 
+    private static class DeletingVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.deleteIfExists(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.deleteIfExists(dir);
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
