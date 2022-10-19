a57ac88e05cf505a4e7d9f7206a64ba8672732ed
SirMathhman <SirMathhman@gmail.com>
2022-10-19 15:31:12 -0400

    Add file generation

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,15 @@ mod tests {
     use std::path::Path;
 
     #[test]
-    fn test() {
+    fn does_not_generate() {
         let path = Path::new("Index.mgs");
         assert_eq!(true, File::open(path).is_err());
+    }
+
+    #[test]
+    fn generates() {
+        let path = Path::new("Index.mgs");
+        File::create(path).unwrap();
+        assert_eq!(false, File::open(path).is_err())
     }
 }
