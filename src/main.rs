50dc10dc65b76214437b8a3338c80ec83913a635
SirMathhman <SirMathhman@gmail.com>
2022-10-19 15:21:47 -0400

    Ensure file does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::path::Path;
+
+    #[test]
+    fn test() {
+        let path = Path::new("Index.mgs");
+        assert_eq!(true, File::open(path).is_err());
+    }
+}
