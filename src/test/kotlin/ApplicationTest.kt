19f4e1bcc5727a8e4d903fb795bd9c1f12d5bffc
SirMathhman <SirMathhman@gmail.com>
2023-01-11 16:09:24 -0500

    Add target generation

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,25 +1,41 @@
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
 import java.nio.file.Path
 
 class ApplicationTest {
-    private lateinit var path: Path
+    private lateinit var working: Path
 
     @BeforeEach
     fun setUp() {
-        path = Files.createTempDirectory("working")
+        working = Files.createTempDirectory("working")
     }
 
     @AfterEach
     fun tearDown() {
-        Files.walkFileTree(path, DeletingVisitor)
+        Files.walkFileTree(working, DeletingVisitor)
     }
 
     @Test
-    fun test() {
-        assertFalse(Files.exists(path))
+    fun generates_target() {
+        Files.createFile(resolveSource())
+        if (Files.exists(resolveSource())) {
+            Files.createFile(resolveTarget())
+        }
+        assertTrue(doesTargetExist())
     }
+
+    private fun resolveSource(): Path = working.resolve("Index.kt")
+
+    @Test
+    fun generates_no_target() {
+        assertFalse(doesTargetExist())
+    }
+
+    private fun doesTargetExist() = Files.exists(resolveTarget())
+
+    private fun resolveTarget(): Path = working.resolve("Index.mgs")
 }
